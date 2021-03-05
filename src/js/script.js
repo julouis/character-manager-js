362974d7db42e98289205b6ef6ba1134ca5b193e
Julien Louis <julienlouis.becode@gmail.com>
2021-03-05 14:05:54 +0100

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,17 @@
-console.log("wait async");
+
+	// IMAGE TO URL
+	let image = ""
+	document.querySelector("#hero-image-post").addEventListener("change",(e) => {
+		const file = e.target.files[0];
+		const reader = new FileReader();
+		reader.onloadend = () => {
+			image = reader.result.replace(/^data:image\/[a-z]+;base64,/, "");
+			console.log(image)
+		};
+		reader.readAsDataURL(file)
+	});
+	//
+	
 
 var template = document.querySelector("#template");
 var target = document.querySelector("#target");
@@ -14,54 +27,66 @@ async function fetchData() {
 		cloneHero.querySelector("#hero-short-description").innerHTML = `${shortDescription}`;
 		cloneHero.querySelector("#hero-description").innerHTML = `${description}`;
         cloneHero.querySelector("#hero-image").innerHTML = `<img src="data:image/gif;base64,${image}" width="200" height="200">`;
-        
-		// MODIFY FUNCTION
+        cloneHero.querySelector("#modify-button");
 		
-		cloneHero.querySelector("#modify-button");
+		// MODIFY FUNCTION //
+		
+		const modifyInputs = Array.from(document.querySelector("#exampleModalModify input"));
+
+		document.querySelector("#exampleModalModify #create-hero").addEventListener("click",async function() {
+			const values = modifyInputs.map(({value}) => value.trim());	
+			console.log(values)	
+			const [name, shortDescription, description] = values;
+			
+			console.log("Try to modify input")
+
+			const response = await fetch(`https://character-database.becode.xyz/characters${id}`, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({
+					name,
+					shortDescription,
+					description,
+					image
+				})
+				
+			})
+			
+			document.location.reload();
+			
+			console.log(response.status)
+			const modifyHero = await response.json();
+			console.log(modifyHero)
+		
+		})
 
 		// DELETE FUNCTION
-		cloneHero.querySelector("#delete-button").addEventListener("click", () => {
+
+		cloneHero.querySelector("#delete-button").addEventListener("click",async () => {
 			console.log(id);
-			const response = fetch(`https://character-database.becode.xyz/characters/${id}`, {
+			const response =await fetch(`https://character-database.becode.xyz/characters/${id}`, {
 				method: "DELETE",
 				headers: {
                     "Content-Type": "application/json",
 				},
 			});
+				document.location.reload();
 			
 		});
-
 		
-
 		target.appendChild(cloneHero); 
 
-		/* cloneHero.getElementById("delete-button").addEventListener("click",async function (){
-			console.log("test delete")
-			const deleteID = delete data[id];
-		}) */
-
-    console.log(data)
     });
 }
 
 
 fetchData();
 
-	// IMAGE TO URL
-let image = ""
-	document.querySelector("#hero-image-post").addEventListener("change",(e) => {
-		const file = e.target.files[0];
-		const reader = new FileReader();
-		reader.onloadend = () => {
-			image = reader.result.replace(/^data:image\/[a-z]+;base64,/, "");
-			console.log(image)
-		};
-		reader.readAsDataURL(file)
-	});
-	//
-	
+// POST ELEMENT
 
-const inputs = Array.from(document.querySelectorAll(".modal-body input"));
+const inputs = Array.from(document.querySelectorAll("#exampleModal input"));
 
 document.getElementById("create-hero").addEventListener("click", async function() {
 
@@ -93,3 +118,7 @@ document.getElementById("create-hero").addEventListener("click", async function(
 	console.log(newHero)
 
 })
+
+
+// MODIFY ELEMENT 
+
